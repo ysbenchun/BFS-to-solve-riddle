6cd5f40f4fb4055a992ab8837f99440d56a4baf6
Ben Chun <ysbenchun@gmail.com>
2024-09-18 01:35:33 -0400

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,6 +1,5 @@
 class SemanticNetsAgent:
     def __init__(self):
-        #If you want to do any initial processing, add it here.
         pass
     
     # Tester for valid states
@@ -72,16 +71,6 @@ class SemanticNetsAgent:
         return possibleMoves, nextPossibleStates
 
     def solve(self, initial_sheep, initial_wolves):
-        #Add your code here! Your solve method should receive
-        #the initial number of sheep and wolves as integers,
-        #and return a list of 2-tuples that represent the moves
-        #required to get all sheep and wolves from the left
-        #side of the river to the right.
-        #
-        #If it is impossible to move the animals over according
-        #to the rules of the problem, return an empty list of
-        #moves.
-
         # Using BFS (Breadth First Search) algorithm to find the shortest path from initial state to state(0,0)
         visitedStates = {}
         queueState =[(initial_sheep, initial_wolves, "Left")]
